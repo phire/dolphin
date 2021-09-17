adff4bb65b1d71c8762f14c9b7ca76623b9d2322
Scott Mansell <phiren@gmail.com>
2021-09-18 04:27:42 +1200

    Add a lua script host

---
+++ Source/LuaScriptHost/plugin_main.cpp
@@ -0,0 +1,29 @@
+// Copyright 2021 Dolphin Emulator Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include <plugin.h>
+
+#include <stdio.h>
+
+// We have lua compiled as c++, so this works
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+
+static void RunLua() {
+    lua_State *L = luaL_newstate();
+    luaL_openlibs(L);
+
+    lua_close(L);
+}
+
+
+extern "C" {
+
+EXPORTED void plugin_init() {
+    RunLua();
+
+    printf("LuaScriptHost initialized\n");
+}
+
+}
