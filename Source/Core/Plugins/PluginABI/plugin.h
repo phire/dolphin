cd6c048eab720850a1f20cce3d5e09dde12a09a3
Scott Mansell <phiren@gmail.com>
2021-09-18 04:27:42 +1200

    Add a lua script host

--- Source/Core/Plugins/PluginABI/plugin.h
+++ Source/Core/Plugins/PluginABI/plugin.h
@@ -1,4 +1,13 @@
+// Copyright 2021 Dolphin Emulator Project
+// SPDX-License-Identifier: GPL-2.0-or-later#pragma once
+
 #pragma once
+
+#ifdef _WIN32
+#define EXPORTED __declspec(dllexport)
+#else
+#define EXPORTED __attribute__ ((visibility ("default")))
+#endif
 
 extern "C" {
     void plugin_init();
