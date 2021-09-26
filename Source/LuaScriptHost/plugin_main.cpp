8b0419d03c3d09c5e13f900d5b4b073ce65e93bc
Scott Mansell <phiren@gmail.com>
2021-09-26 15:40:02 +1300

    LuaHost: Move Function Wrapper out of main
    Also change it to use userdata for it's storage

--- Source/LuaScriptHost/plugin_main.cpp
+++ Source/LuaScriptHost/plugin_main.cpp
@@ -24,41 +24,6 @@
 
 static uint64_t mod_id;
 
-struct GlobalFunction {
-    std::vector<std::function<void (lua_State *L, int32_t idx, ArgHolder& holder)>> Args;
-    void *(*FnPtr)(void);
-    std::function<void (lua_State *L, void*)> ReturnHandler;
-};
-std::vector<std::unique_ptr<GlobalFunction>> GlobalFunctions;
-
-// This is a generic function that wraps any c function from the API
-static int FunctionWrapper(lua_State *L) {
-    // First, we need to work out what function was actually called
-
-    auto upvalue = lua_upvalueindex(1);
-    if (!lua_islightuserdata(L, upvalue)) {
-        // todo: error handling?
-        return -1;
-    }
-
-    auto FnInfo = reinterpret_cast<GlobalFunction*>(lua_touserdata(L, upvalue));
-
-    std::array<ArgHolder, 20> args;
-    uint32_t arg_count = 0;
-
-    for (auto& arg_fn : FnInfo->Args) {
-        arg_fn(L, arg_count+1, args[arg_count]);
-        arg_count++;
-    }
-
-    void* result = CallWithArgs[arg_count](FnInfo->FnPtr, args.data());
-
-    if (FnInfo->ReturnHandler) {
-        FnInfo->ReturnHandler(L, result);
-    }
-
-    return 1;
-}
 
 static void RunLua() {
     lua_State *L = luaL_newstate();
@@ -86,7 +51,9 @@ static void RunLua() {
 
             //printf("    %s\n", Func.FunctionName);
 
-            auto ptr = GlobalFunctions.emplace_back(std::make_unique<GlobalFunction>()).get();
+            auto ptr = reinterpret_cast<FunctionInfo*>(lua_newuserdatauv(L, sizeof(FunctionInfo), 0));
+            new (ptr) FunctionInfo();
+
             ptr->FnPtr = Func.FnPtr;
             for (uint32_t k = 0; k < Func.ArgumentCount; k++) {
                 Argument& Arg = Func.Arguments[k];
@@ -98,7 +65,6 @@ static void RunLua() {
             }
             ptr->ReturnHandler = LookupType(Func.ReturnType).AccessHandler;
 
-                lua_pushlightuserdata(L, ptr);
                 lua_pushcclosure(L, &FunctionWrapper, 1);
                 lua_setfield(L, -2, Func.FunctionName);
         }
