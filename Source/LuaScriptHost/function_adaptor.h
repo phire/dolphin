1982cfe361f7226fe6d6c7fa356e35243f29e8bc
Scott Mansell <phiren@gmail.com>
2021-09-26 15:04:47 +1300

    LuaHost: Add a type database

--- Source/LuaScriptHost/function_adaptor.h
+++ Source/LuaScriptHost/function_adaptor.h
@@ -6,10 +6,6 @@
 #include <array>
 #include <cstdint>
 
-struct ArgHolder
-{
-    void* val; // The actual value which will based passed to the called function
-    uint8_t extra_data_storage[32]; // A bit of storage in case an arg type requires building a struct
-};
+struct ArgHolder;
 
 extern std::array<void*(*)(void*(*)(void), ArgHolder *), 21> CallWithArgs;
