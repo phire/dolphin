b85404c0a2677aec331bc91a4d7829c94137d7fd
Scott Mansell <phiren@gmail.com>
2026-07-03 18:57:07 +1200

    Fix the WIP CPU API

--- Source/Core/Core/PowerPC/CPUApi.cpp
+++ Source/Core/Core/PowerPC/CPUApi.cpp
@@ -5,6 +5,9 @@
 #include "Common/CommonTypes.h"
 #include "Core/CoreTiming.h"
 #include "Core/PowerPC/CPUApi.h"
+#include "Core/System.h"
+#include "Core/Core.h"
+
 
 #include <climits>
 #include <compare>
@@ -18,109 +21,121 @@
 
 
 
-static void BreakOnCycleEvent(u64 userdata, s64 cyclesLate);
+static void BreakOnCycleEvent(Core::System& system, u64 userdata, s64 cyclesLate);
 
 namespace CpuApi {
 
+struct CpuMemory;
+
 static u64 CurrentHandle = 0;
 static bool s_initialized = false;
-static std::vector<Functor<void (CpuMemory, u64)>> BreakOnRun;
+static std::vector<Functor<void (Core::System&, CpuMemory*, u64)>> BreakOnRun;
 
 ZAP_NAMESPACE_MODULE("Cpu", Zap::VersionInfo(1, 1), "Allows accessing CPU state and memory");
 
 struct CpuMemory {
-  ZAP_CHECKED_HANDLE(CpuMemory);
+  //ZAP_CHECKED_HANDLE(CpuMemory);
 
-  u32 ReadU8(u32 address)
-  {
-    return PowerPC::HostRead_U8(address);
+  // u32 ReadU8(u32 address)
+  // {
+  //   return PowerPC::MMU::HostRead<u8>(m_cpu_guard, address);
+  // }
+
+  // u32 ReadU16(u32 address)
+  // {
+  //   return PowerPC::MMU::HostRead<u16>(m_cpu_guard, address);
+  // }
+
+  // u32 ReadU32(u32 address)
+  // {
+  //   return PowerPC::MMU::HostRead<u32>(m_cpu_guard, address);
+  // }
+
+  // u64 ReadU64(u32 address)
+  // {
+  //   return PowerPC::MMU::HostRead<u64>(m_cpu_guard, address);
+  // }
+
+  // float ReadFloat(u32 address) {
+  //   return PowerPC::MMU::HostRead<float>(m_cpu_guard, address);
+  // }
+
+  // double CpuMemory_ReadDouble(u32 address)
+  // {
+  //   return PowerPC::MMU::HostRead<double>(m_cpu_guard, address);
+  // }
+
+  // void CpuMemory_WriteU8(u32 address, u8 value) {
+
+  //   PowerPC::MMU::HostWrite<u8>(m_cpu_guard, value, address);
+  // }
+
+  // void CpuMemory_WriteU16(u32 address, u16 value)
+  // {
+  //   PowerPC::MMU::HostWrite<u16>(m_cpu_guard, value, address);
+  // }
+
+  // void CpuMemory_WriteU32(u32 address, u32 value)
+  // {
+  //   PowerPC::MMU::HostWrite<u32>(m_cpu_guard, value, address);
+  // }
+
+  // void CpuMemory_WriteU64(u32 address, u64 value)
+  // {
+  //   PowerPC::MMU::HostWrite<u64>(m_cpu_guard, value, address);
+  // }
+
+  // void CpuMemory_WriteFloat(u32 address, float value)
+  // {
+  //   PowerPC::MMU::HostWrite<float>(m_cpu_guard, value, address);
+  // }
+
+  // void CpuMemory_WriteDouble(u32 address, double value)
+  // {
+  //   PowerPC::MMU::HostWrite<double>(m_cpu_guard, value, address);
+  // }
+
+  void CpuMemory_BreakOnCycle(s64 CyclesIntoFuture, Functor<void (Core::System&, CpuMemory*, u64)> callback) {
+    u64 Userdata = std::bit_cast<u64>(callback);
+    Core::System::GetInstance().GetCoreTiming().ScheduleAnonymousEvent(CyclesIntoFuture, BreakOnCycleEvent, Userdata);
   }
 
-  u32 ReadU16(u32 address)
-  {
-    return PowerPC::HostRead_U16(address);
-  }
+  Core::CPUThreadGuard& m_cpu_guard;
 
-  u32 ReadU32(u32 address)
-  {
-    return PowerPC::HostRead_U32(address);
-  }
-
-  u64 ReadU64(u32 address)
-  {
-    return PowerPC::HostRead_U64(address);
-  }
-
-  float ReadFloat(u32 address) {
-    return PowerPC::HostRead_F32(address);
-  }
-
-  double CpuMemory_ReadDouble(u32 address)
-  {
-    return PowerPC::HostRead_F64(address);
-  }
-
-  void CpuMemory_WriteU8(u32 address, u8 value) {
-
-    PowerPC::HostWrite_U8(value, address);
-  }
-
-  void CpuMemory_WriteU16(u32 address, u16 value)
-  {
-    PowerPC::HostWrite_U16(value, address);
-  }
-
-  void CpuMemory_WriteU32(u32 address, u32 value)
-  {
-    PowerPC::HostWrite_U32(value, address);
-  }
-
-  void CpuMemory_WriteU64(u32 address, u64 value)
-  {
-    PowerPC::HostWrite_U64(value, address);
-  }
-
-  void CpuMemory_WriteFloat(u32 address, float value)
-  {
-    PowerPC::HostWrite_F32(value, address);
-  }
-
-  void CpuMemory_WriteDouble(u32 address, double value)
-  {
-    PowerPC::HostWrite_F64(value, address);
-  }
-
-  void CpuMemory_BreakOnCycle(s64 CyclesIntoFuture, Functor<void (CpuMemory, u64)> callback) {
-    u64 Userdata = Common::BitCast<u64>(callback);
-    CoreTiming::ScheduleAnonymousEvent(CyclesIntoFuture, BreakOnCycleEvent, Userdata);
-  }
+  CpuMemory(Core::CPUThreadGuard& guard) : m_cpu_guard(guard) {}
 };
 
 
 
-void Cpu_BreakOnRun(Functor<void (CpuMemoryHandle, u64)> callback) {
+void Cpu_BreakOnRun(Functor<void (Core::System&, CpuMemory*, u64)> callback) {
   // TODO: Should this be callable from threads other than CPU?
   //       Currenty it's not
   //       How are we going to make sure this is only callable from the proper thread?
   if (s_initialized) {
     CurrentHandle += 1;
-    callback(CurrentHandle, CoreTiming::GetTicks());
+    u64 ticks = Core::System::GetInstance().GetCoreTiming().GetTicks();
+    auto& system = Core::System::GetInstance();
+    auto guard = Core::CPUThreadGuard(system);
+    CpuMemory cpu_memory_handle(guard);
+    callback(system, &cpu_memory_handle, ticks);
   } else {
     BreakOnRun.push_back(callback);
   }
 }
 
-static void InitCallback(u64 userdata, s64 cycles_late) {
+static void InitCallback(Core::System& system, u64 userdata, s64 cycles_late) {
   CurrentHandle += 1;
+  auto guard = Core::CPUThreadGuard(system);
+  auto cpu_memory_handle = CpuApi::CpuMemory (guard);
   for(auto callback : BreakOnRun) {
-    callback(CurrentHandle, static_cast<u64>(cycles_late));
+    callback(system, &cpu_memory_handle, static_cast<u64>(cycles_late));
   }
   BreakOnRun.clear();
   s_initialized = true;
 }
 
 ZAP_IGNORE void Init() {
-  CoreTiming::ScheduleAnonymousEvent(0, InitCallback, 0);
+  Core::System::GetInstance().GetCoreTiming().ScheduleAnonymousEvent(0, &InitCallback, 0);
 }
 
 ZAP_IGNORE void Shutdown() {
@@ -129,9 +144,11 @@ ZAP_IGNORE void Shutdown() {
 
 } // namespace CpuApi
 
-static void BreakOnCycleEvent(u64 userdata, s64 cyclesLate) {
-  auto callback = Common::BitCast<Functor<void (CpuApi::CpuMemory*, u64)>>(userdata);
-  CpuApi::CpuMemory cpu_memory_handle;
+static void BreakOnCycleEvent(Core::System& system, u64 userdata, s64 cyclesLate) {
+  auto callback = std::bit_cast<Functor<void (Core::System&, CpuApi::CpuMemory*, u64)>>(userdata);
+  auto guard = Core::CPUThreadGuard(system);
+  auto cpu_memory_handle = CpuApi::CpuMemory (guard);
 
-  callback(&cpu_memory_handle, static_cast<u64>(cyclesLate));
+  callback(system, &cpu_memory_handle, static_cast<u64>(cyclesLate));
 }
+
